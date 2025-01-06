1e23ecb3cd69b4ed32679c81c6b29ba32c90549d
TrinityFate <simon.kodu@gmail.com>
2025-01-06 14:09:23 +0100

    initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "com.example.lightingsystemv2"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.lightingsystemv2"
+        minSdk = 24
+        //noinspection ExpiredTargetSdkVersion
+        targetSdk = 30
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+    implementation("org.eclipse.paho:org.eclipse.paho.client.mqttv3:1.1.0")
+    implementation("org.eclipse.paho:org.eclipse.paho.android.service:1.1.1")
+    implementation("androidx.legacy:legacy-support-v4:1.0.0")
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+}
---
+++ app/src/main/java/com/example/lightingsystemv2/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.lightingsystemv2;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView txv_rgb;
+    private TextView txv_light;
+    private TextView txv_proximity;
+    private Button btn_color;
+    private MqttAndroidClient client;
+    private static final String SERVER_URI = "tcp://test.mosquitto.org:1883";
+    private static final String TAG = "MainActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        //txv_rgb = (TextView) findViewById(R.id.txv_rgbValue);
+//        txv_light = (TextView) findViewById(R.id.txv_lightValue);
+//        txv_proximity = (TextView) findViewById(R.id.txv_proximityValue);
+//        btn_color = (Button) findViewById(R.id.btnColor);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    private void connect(){
+        String clientId = MqttClient.generateClientId();
+        client = new MqttAndroidClient(this.getApplicationContext(), SERVER_URI, clientId);
+        try {
+            IMqttToken token = client.connect();
+            token.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    Log.d(TAG, "onSuccess");
+                    System.out.println(TAG + " Success. Connected to " + SERVER_URI);
+                }
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    Log.d(TAG, "onFailure");
+                    System.out.println(TAG + " Oh no! Failed to connect to " + SERVER_URI);
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+}
